1dc9ebfd8203ec9e520f1c530112029bce0cef6a
Amber Elferink <a.s.elferink@students.uu.nl>
2019-01-10 18:13:01 +0100

    Alle pagina routings werken

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Portfolio', stitle: "Hi, I'm Amber"} );
 });
 
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'All Projects', stitle: ''});
+});
+
+//contact werkt niet, maar / en /projects wel?
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact', stitle: 'Feel free to send me an email'});
+});
+
+
 module.exports = router;
+
