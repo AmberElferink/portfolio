180af94146b041a429eb6068d01b30dda04e2618
Amber Elferink <a.s.elferink@students.uu.nl>
2019-01-20 13:47:47 +0100

    error message mail opgeschoond

--- routes/index.js
+++ routes/index.js
@@ -20,15 +20,21 @@ router.get('/contact', function(req, res, next) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const msg = {
-    to: 'tomail@gmail.com',
-    from: 'frommail@gmail.com',
+    to: 'toemail@gmail.com',
+    from: 'fromemail@gmail.com',
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
   sgMail.send(msg, function(err, json) {
-    if (err) {return res.send('AAAAAH!'); }
-    res.send('WOOHOO!!')
+    if (err) {
+      return res.render('contactResponse', 
+        {title: 'An error with sending the email has occured. Please try again later or contact me via LinkedIn.'});
+    }
+
+    res.render('contactResponse', 
+    {title: 'Thank you for your email. I will respond as soon as possible.'});
+    
   }); 
 });
 
