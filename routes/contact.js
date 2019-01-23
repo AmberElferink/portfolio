90a60688e6110f1c080619d2d50bce00143479ea
Amber Elferink <a.s.elferink@students.uu.nl>
2019-01-23 23:13:11 +0100

    javascript form aan server gekoppeld

--- routes/contact.js
+++ routes/contact.js
@@ -10,18 +10,27 @@ console.log("contact");
 
 
 router.post('/', function(req, res, next) {
+  var host = req.get('host');
+  console.log('host ' + host + " sent an email from email address: " + req.body.email);
+  console.log('content: ' + req.body.content);
+  if(host != 'localhost:3000')
+  {
+    res.send("Please use the original website to send me an email.")
+    return;
+  }
   const sgMail = require('@sendgrid/mail');
-  console.log(req.url);
 
+  //this reads the secret key from the process environment
+  //the key was loaded in there by a .env file in the root folder of the project
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const msg = {
-    to: 'toemail@gmail.com',
-    from: 'fromemail@gmail.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    to: 'myemail@gmail.com',
+    from: req.body.email,
+    subject: req.body.subject,
+    text: req.body.content,
   };
+  console.log(msg);
   /*
   sgMail.send(msg, function(err, json) {
     if (err) {
