76c6bc117d815578814f4e9593d4bd1b486a4f1e
Amber Elferink <a.s.elferink@students.uu.nl>
2019-02-14 18:46:33 +0100

    aanpassen if else vanwege meerdere res op req

--- routes/contact.js
+++ routes/contact.js
@@ -51,68 +51,78 @@ router.post('/', function(req, res, next) {
         subject: req.body.subject, 
         content: req.body.content, 
         errs: [{msg: "Failed captcha verification, try again."}]
-      });   }
+      });   
+    }
+
+
+    else {
+        //--------------------------------------FORM VALIDATION PART----------------------------------------------------
+        req.checkBody('name', 'name is required').notEmpty().isLength({min: 3}).escape();
+        req.checkBody('email', 'email must be a valid email format').notEmpty().isEmail().normalizeEmail();
+        req.checkBody('subject', 'subject is required').notEmpty().escape();
+        req.checkBody('content', 'content is required').notEmpty().trim().escape();
+
+        var errors = req.validationErrors();
+        console.log(errors);
+        if(errors) {
+          console.log(errors);
+          return res.render('contact', {
+            title: "Let's talk!", 
+            name: req.body.name, 
+            email: req.body.email, 
+            subject: req.body.subject, 
+            content: req.body.content, 
+            errs: errors
+          });
+        }
+        else {
+            ///----------------------------------MESSAGE SENDING PART---------------------------------------------------------
+          const msg = {
+            to: 'someemail@something.com',
+            from: {
+              email: req.body.email,
+              name: req.body.name
+            },
+            subject: req.body.subject,
+            text: req.body.content + ' \r\n sent from portfolio site www.amber-elferink.com.',
+          };
+          var host = req.get('host');
+          winston.info(`host: ${host} sent a message from - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+          winston.info(msg);
+
+          //if(host != 'localhost:3000')
+          //{
+          // res.send("Please use the original website to send me an email.")
+          // winston.warn(`above message was not sent from the original website and has not been sent to mail`)
+          // return;
+          //}
+          const sgMail = require('@sendgrid/mail');
+
+          //this reads the secret key from the process environment
+          //the key was loaded in there by a .env file in the root folder of the project
+          sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+          
+          sgMail.send(msg, function(err, json) {
+            if (err) {
+              return res.render('contactResponse', 
+                {title: 'An error with sending the email has occured. Please try again later or contact me via LinkedIn'});
+            }
+            
+            res.render('contactResponse', 
+            {title: 'Thank you for your email. I will respond as soon as possible.'});
+
+          }); 
+        }
+    }
   });
 
 
-  //--------------------------------------FORM VALIDATION PART----------------------------------------------------
-  req.checkBody('name', 'name is required').notEmpty().isLength({min: 3}).escape();
-  req.checkBody('email', 'email must be a valid email format').notEmpty().isEmail().normalizeEmail();
-  req.checkBody('subject', 'subject is required').notEmpty().escape();
-  req.checkBody('content', 'content is required').notEmpty().trim().escape();
 
-  var errors = req.validationErrors();
-  console.log(errors);
-  if(errors) {
-    console.log(errors);
-    return res.render('contact', {
-      title: "Let's talk!", 
-      name: req.body.name, 
-      email: req.body.email, 
-      subject: req.body.subject, 
-      content: req.body.content, 
-      errs: errors
-    });
-  }
-
-  ///----------------------------------MESSAGE SENDING PART---------------------------------------------------------
-  const msg = {
-    to: 'amber.elferink@gmail.com',
-    from: {
-      email: req.body.email,
-      name: req.body.name
-    },
-    subject: req.body.subject,
-    text: req.body.content + ' \r\n sent from portfolio site www.amber-elferink.com.',
-  };
-  var host = req.get('host');
-  winston.info(`host: ${host} sent a message from - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-  winston.info(msg);
-
-  //if(host != 'localhost:3000')
-  //{
-   // res.send("Please use the original website to send me an email.")
-   // winston.warn(`above message was not sent from the original website and has not been sent to mail`)
-   // return;
-  //}
-  const sgMail = require('@sendgrid/mail');
-
-  //this reads the secret key from the process environment
-  //the key was loaded in there by a .env file in the root folder of the project
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
   
-  sgMail.send(msg, function(err, json) {
-    if (err) {
-      return res.render('contactResponse', 
-        {title: 'An error with sending the email has occured. Please try again later or contact me via LinkedIn'});
-    }
-    
-    res.render('contactResponse', 
-    {title: 'Thank you for your email. I will respond as soon as possible.'});
-
-  }); 
 
 });
 
