d675e26d6bf694d8dce79cc04f7de6ab5f25a069
Amber Elferink <a.s.elferink@students.uu.nl>
2019-02-17 16:48:19 +0100

    werkt volledig met callbacks local

--- routes/contact.js
+++ routes/contact.js
@@ -18,7 +18,24 @@ router.post('/', function(req, res, next) {
   checkRECAPTCHA(function(err, message){
     if(message === "allgood")
     {
-      sendEmail();
+      validateForm(function(validateErrs){
+        if(validateErrs)
+        {
+          console.error(validateErrs);
+          return res.render('contact', {
+            title: "Let's talk!", 
+            name: req.body.name, 
+            email: req.body.email, 
+            subject: req.body.subject, 
+            content: req.body.content, 
+            errs: validateErrs
+          });
+        }
+        else 
+        {
+          sendEmail(req, res);
+        }
+      }, req);
     }
     else
     {
@@ -67,28 +84,28 @@ router.post('/', function(req, res, next) {
     }
   }
 
-/*
-  //--------------------------------------FORM VALIDATION PART----------------------------------------------------
-  req.checkBody('name', 'name is required').notEmpty().isLength({min: 3}).escape();
-  req.checkBody('email', 'email must be a valid email format').notEmpty().isEmail().normalizeEmail();
-  req.checkBody('subject', 'subject is required').notEmpty().escape();
-  req.checkBody('content', 'content is required').notEmpty().trim().escape();
 
-  var errors = req.validationErrors();
-  console.log(errors);
-  if(errors) {
-    console.log(errors);
-    return res.render('contact', {
-      title: "Let's talk!", 
-      name: req.body.name, 
-      email: req.body.email, 
-      subject: req.body.subject, 
-      content: req.body.content, 
-      errs: errors
-    });
+  //--------------------------------------FORM VALIDATION PART----------------------------------------------------
+  function validateForm(callback, req) {
+    req.checkBody('name', 'name is required').notEmpty().isLength({min: 3}).escape();
+    req.checkBody('email', 'email must be a valid email format').notEmpty().isEmail().normalizeEmail();
+    req.checkBody('subject', 'subject is required').notEmpty().escape();
+    req.checkBody('content', 'content is required').notEmpty().trim().escape();
+
+    var errors = req.validationErrors();
+
+    if(errors) {
+      callback(errors);
+    }
+    else {
+      callback(undefined);
+    }
   }
+  
 
   ///----------------------------------MESSAGE SENDING PART---------------------------------------------------------
+ 
+function sendEmail(req, res) {
   const msg = {
     to: 'amber.elferink@gmail.com',
     from: {
@@ -102,35 +119,22 @@ router.post('/', function(req, res, next) {
   winston.info(`host: ${host} sent a message from - ${req.originalUrl} - ${req.method} - ${req.ip}`);
   winston.info(msg);
 
-  //if(host != 'localhost:3000')
-  //{
-   // res.send("Please use the original website to send me an email.")
-   // winston.warn(`above message was not sent from the original website and has not been sent to mail`)
-   // return;
-  //}
   const sgMail = require('@sendgrid/mail');
 
   //this reads the secret key from the process environment
   //the key was loaded in there by a .env file in the root folder of the project
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-
-  
   sgMail.send(msg, function(err, json) {
     if (err) {
       return res.render('contactResponse', 
         {title: 'An error with sending the email has occured. Please try again later or contact me via LinkedIn'});
     }
-    
-    res.render('contactResponse', 
-    {title: 'Thank you for your email. I will respond as soon as possible.'});
-
+    else {
+      res.render('contactResponse', 
+      {title: 'Thank you for your email. I will respond as soon as possible.'});
+    }
   }); 
-
-});
-*/
-function sendEmail() {
-  console.log("sending email test");
 }
 
 module.exports = router;
