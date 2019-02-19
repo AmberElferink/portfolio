281247e0825f5ded2540a754ad96c94527c74e6e
Amber Elferink <a.s.elferink@students.uu.nl>
2019-02-19 19:27:09 +0100

    carousel doet het nu ineens wel

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -1,8 +1,8 @@
 // When the DOM is ready, run this function
-$(document).ready(function() {
+window.onload = function() {
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
     interval: 30000,
   });
-});
+};
