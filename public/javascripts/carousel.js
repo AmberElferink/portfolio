5c33a32a0a4a59b9c6851e5bfdee2495b259664b
Amber Elferink <a.s.elferink@students.uu.nl>
2019-02-21 00:56:33 +0100

    even less I's

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -1,8 +1,9 @@
 // When the DOM is ready, run this function
-window.onload = function() {
+window.addEventListener('load', function(event) {
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
     interval: 30000,
   });
-};
+});
+
