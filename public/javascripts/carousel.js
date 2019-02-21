ba4431a4c7216f9829febf45ebceec38b93adb19
Amber Elferink <a.s.elferink@students.uu.nl>
2019-02-21 16:02:58 +0100

    removed carousel.js

--- public/javascripts/carousel.js
+++
@@ -1,9 +0,0 @@
-// When the DOM is ready, run this function
-window.addEventListener('load', function(event) {
-  //Set the carousel options
-  $('#quote-carousel').carousel({
-    pause: true,
-    interval: 30000,
-  });
-});
-
